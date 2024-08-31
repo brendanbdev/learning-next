35ba5c6ce3cdff966ddebc2183283ebe8ecb1782
brendanbdev <111916626+brendanbdev@users.noreply.github.com>
2024-08-31 17:38:23 -0400

    added comment to auth.ts with link to auth error handling documentation

--- auth.ts
+++ auth.ts
@@ -1,3 +1,4 @@
+// Handle auth errors: https://authjs.dev/reference/core/errors
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
